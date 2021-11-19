184da61bcea5a4af4338a80ab213efc5aa682248
scshil <chandrasaurav20@gmail.com>
2021-11-19 19:05:51 +0600

    solved

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 //
-// const port = process.env.PORT || 5000;
-const port = 5000;
+const port = process.env.PORT || 5000;
+// const port = 5000;
 app.use(cors());
 app.use(express.json());
 //
